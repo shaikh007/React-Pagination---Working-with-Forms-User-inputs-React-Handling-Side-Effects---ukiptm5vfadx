0f5d96f0d970357227da80f4334ed4faf2c75a37
Shaikh Irfan <shaikhji1998@gmail.com>
2022-12-14 23:28:50 +0530

    new Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import '../styles/App.css';
-import { PostList } from './PostList';
+import React from "react";
+import "../styles/App.css";
+import { PostList } from "./PostList";
 const App = () => {
-
   return (
     <div id="main">
       <PostList />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
